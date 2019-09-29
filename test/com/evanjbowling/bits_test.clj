05f915744a2e82299322f5976adbb75b37dc7dd1
Evan Bowling <evan.j.bowling@gmail.com>
2019-09-29 14:35:37 -0500

    Add test coverage for double fns

--- test/com/evanjbowling/bits_test.clj
+++ test/com/evanjbowling/bits_test.clj
@@ -62,6 +62,21 @@
     "0.1"                   ["0" "01111011" "10011001100110011001101"]
     "9"                     ["0" "10000010" "00100000000000000000000"]))
 
+(deftest test-double-bits
+  (are [d expected]
+       (->> (b/double-bits d)
+            (map (partial string/join ""))
+            ((partial = expected)))
+    Double/POSITIVE_INFINITY ["0" "11111111111" "0000000000000000000000000000000000000000000000000000"]
+    Double/NEGATIVE_INFINITY ["1" "11111111111" "0000000000000000000000000000000000000000000000000000"]
+    Double/MAX_VALUE         ["0" "11111111110" "1111111111111111111111111111111111111111111111111111"]
+    Double/MIN_NORMAL        ["0" "00000000001" "0000000000000000000000000000000000000000000000000000"]
+    Double/MIN_VALUE         ["0" "00000000000" "0000000000000000000000000000000000000000000000000001"]
+    "0.0"                    ["0" "00000000000" "0000000000000000000000000000000000000000000000000000"]
+    "-0.0"                   ["1" "00000000000" "0000000000000000000000000000000000000000000000000000"]
+    "9007199254740992"       ["0" "10000110100" "0000000000000000000000000000000000000000000000000000"]
+    "9007199254740993"       ["0" "10000110100" "0000000000000000000000000000000000000000000000000000"]))
+
 (deftest test-float-attrs
   (is (= Float/MIN_EXPONENT (:exponent (b/float-attrs Float/MIN_VALUE))))
   (is (= Float/MAX_EXPONENT (:exponent (b/float-attrs Float/MAX_VALUE))))
@@ -117,5 +132,58 @@
     {:negative? false
      :type      :infinity}))
 
-;; TODO: test-double-bits
+(deftest test-double-attrs
+  (is (= Double/MIN_EXPONENT (:exponent (b/double-attrs Double/MIN_VALUE))))
+  (is (= Double/MAX_EXPONENT (:exponent (b/double-attrs Double/MAX_VALUE))))
+  (are [d expected] (= expected (b/double-attrs d))
+    Double/MAX_VALUE
+    {:negative?       false
+     :type            :normal
+     :exponent-biased 2046
+     :exponent        1023
+     :fraction        9007199254740991/4503599627370496
+     :rational        179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368N}
+
+    Double/MIN_NORMAL
+    {:negative?       false
+     :type            :normal
+     :exponent-biased 1
+     :exponent        -1022
+     :fraction        1N
+     :rational        1/44942328371557897693232629769725618340449424473557664318357520289433168951375240783177119330601884005280028469967848339414697442203604155623211857659868531094441973356216371319075554900311523529863270738021251442209537670585615720368478277635206809290837627671146574559986811484619929076208839082406056034304}
+
+    Double/MIN_VALUE
+    {:negative?       false
+     :type            :subnormal
+     :exponent-biased 0
+     :exponent        -1022
+     :fraction        1/4503599627370496
+     :rational        1/202402253307310618352495346718917307049556649764142118356901358027430339567995346891960383701437124495187077864316811911389808737385793476867013399940738509921517424276566361364466907742093216341239767678472745068562007483424692698618103355649159556340810056512358769552333414615230502532186327508646006263307707741093494784}
+
+    Double/NaN
+    {:type :nan}
+
+    "0.0"
+    {:negative?       false
+     :type            :zero
+     :exponent-biased 0
+     :exponent        0
+     :fraction        0
+     :rational        0}
+
+    "-0.0"
+    {:negative?       true
+     :type            :zero
+     :exponent-biased 0
+     :exponent        0
+     :fraction        0
+     :rational        0}
+
+    Double/NEGATIVE_INFINITY
+    {:negative? true
+     :type :infinity}
+
+    Double/POSITIVE_INFINITY
+    {:negative? false
+     :type      :infinity}))
 
